4aa4f57783bfc0ba7b8addb227b31aeb8f9bb164
Sdqumar <Sdqumar09.gmail.com>
2021-10-28 00:55:32 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes')
+const cookie = require('cookie-parser')
+const { requireAuth } = require('./middleware/authMiddleware');
+const { currentUser } = require('./middleware/authMiddleware');
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json())
+app.use(cookie())
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://Sadeeq:SadeeqDB@cluster0.nzadu.mongodb.net/NextAuthDB';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) =>{ 
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', currentUser)
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes)
+
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const {isEmail} = require('validator')
+const argon2 = require('argon2');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required:[ true, 'Please enter an email address'],
+    unique: true,
+    lowercase: true,
+    validate:[isEmail, 'please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [ true, 'Please enter a valid password'],
+    minlength: [6, 'Minimum password length should be 6 char'],
+  },
+});
+
+//fire a function after doc saved to db
+// userSchema.post('save', function (doc,next){
+//     console.log('new user was created', doc)
+//     next()
+// })
+//fire a function before doc saved to db
+userSchema.pre('save', async function (next){
+    this.password = await argon2.hash(this.password);
+    next()
+})
+
+//static method to login user
+userSchema.statics.login = async function(email,password){
+    const user = await this.findOne({email})
+    if(user){
+        const auth = await argon2.verify(user.password,password)
+        if(auth){
+            return user
+        }
+
+        throw Error('incorrect password')
+    }
+    throw Error('incorrect email')
+}
+
+const User = mongoose.model('users', userSchema)
+
+
+module.exports = User
