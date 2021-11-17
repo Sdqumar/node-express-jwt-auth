d858c29534bf644b07a7936cba377b8216577034
Sdqumar <sdqumar09@gmail.com>
2021-11-17 23:34:19 +0100

    connect to mongodb

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(cookie())
 app.set('view engine', 'ejs');
 
 // database connection
-// const dbURI = 'mongodb+srv://Sadeeq:SadeeqDB@cluster0.nzadu.mongodb.net/NextAuthDB';
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-//   .then((result) =>{ 
-//     console.log("server started")
-//   })
-//   .catch((err) => console.log(err));
+const dbURI = 'mongodb+srv://Sadeeq:SadeeqDB@cluster0.nzadu.mongodb.net/NextAuthDB';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) =>{ 
+    console.log("server started")
+  })
+  .catch((err) => console.log(err));
 
 // routes
 app.get('*', currentUser)
